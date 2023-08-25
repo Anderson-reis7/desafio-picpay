16366554dc0cf27f0e238253b98b21b88152ace4
Anderson Reis <Anderson.200810@gmail.com>
2023-08-25 11:46:26 -0300

    Criando entidades de usuarios

---
+++ src/main/java/com/picpay/domain/user/User.java
@@ -0,0 +1,33 @@
+package com.picpay.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastname;
+    @Column(unique = true)
+    private String cpf;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+
+
+
+}
